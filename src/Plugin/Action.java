dc6b0e618074df533c5cec58fd7dc2c9821b3ee6
Amirhossein Atashkar <ahatashkar@gmail.com>
2020-06-09 10:48:32 +0430

    add ToolWindowFactory & Action

---
+++ src/Plugin/Action.java
@@ -0,0 +1,12 @@
+package Plugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class Action extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+
+    }
+}
---
+++ src/Plugin/EspressoToolWindowFactory.java
@@ -0,0 +1,11 @@
+package Plugin;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+
+public class EspressoToolWindowFactory implements ToolWindowFactory {
+
+    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+    }
+}
