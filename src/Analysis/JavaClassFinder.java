62f26cace2eb6b0f0e4e353408ced4aa2c976011
Amirhossein Atashkar <ahatashkar@gmail.com>
2020-06-09 11:27:55 +0430

    add JavaClassFinder

---
+++ src/Analysis/JavaClassFinder.java
@@ -0,0 +1,38 @@
+package Analysis;
+
+import com.intellij.psi.JavaRecursiveElementVisitor;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiJavaFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaClassFinder extends JavaRecursiveElementVisitor {
+
+    List<PsiClass> javaClasses;
+
+    public JavaClassFinder() {
+        this.javaClasses = new ArrayList<>();
+    }
+
+    public List<PsiClass> getJavaClasses(){
+        return javaClasses;
+    }
+
+    @Override
+    public void visitJavaFile(PsiJavaFile psiJavaFile) {
+        super.visitFile(psiJavaFile);
+
+        if (psiJavaFile.getName().endsWith(".java") && !psiJavaFile.getName().equals("R.java")) {
+
+            PsiClass[] psiClasses = psiJavaFile.getClasses();
+            for (PsiClass psiClass : psiClasses) {
+                javaClasses.add(psiClass);
+            }
+
+        }
+    }
+
+
+
+}
