762f2a7d0b463383f8caf4e57a3773cbe9330d79
Amirhossein Atashkar <ahatashkar@gmail.com>
2020-06-09 11:28:12 +0430

    add LayoutFinder

---
+++ src/Analysis/LayoutFinder.java
@@ -0,0 +1,28 @@
+package Analysis;
+
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LayoutFinder {
+
+    public List<VirtualFile> getLayouts(VirtualFile layoutDirectory) {
+
+        List<VirtualFile> layoutFiles = new ArrayList<>();
+        VirtualFile[] children = layoutDirectory.getChildren();
+
+        for (VirtualFile child : children) {
+
+            if (child.isDirectory()) {
+                List<VirtualFile> innerLayoutFiles = getLayouts(child);
+                layoutFiles.addAll(innerLayoutFiles);
+
+            } else if (child.getName().toLowerCase().endsWith(".xml")) {
+                layoutFiles.add(child);
+            }
+        }
+
+        return layoutFiles;
+    }
+}
